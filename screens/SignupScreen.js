9ac56b8b1e54bf7e4e6a09092695a5d958240cde
Berke Baklan <berkebaklan@hotmail.com>
2022-09-03 16:03:38 +0300

    gitignore and SignupScreen changes

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,22 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { createUser } from "../util/auth";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const signupHandler = async ({ email, password }) => {
+    setIsAuthenticating(true);
+    await createUser(email, password);
+    setIsAuthenticating(false);
+  };
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="User creating..."/>;
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
